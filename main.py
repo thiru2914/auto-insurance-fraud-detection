bec84a7d835ccc83d14a0ccf9458edb07e053492
thiru2914 <154120187+thiru2914@users.noreply.github.com>
2024-01-23 16:44:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,9 @@ def main():
 
         st.subheader('Uploaded Data:')
         st.write(df)
+        X = df.drop('FraudFound_P', axis=1)
+        y = df['FraudFound_P']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         scaler = StandardScaler()
         X_train_scaled = scaler.fit_transform(X_train)
         X_test_scaled = scaler.transform(X_test)
