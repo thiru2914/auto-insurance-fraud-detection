2898fd8723653e58ed556c30ccc609b66c8cd1ab
thiru2914 <154120187+thiru2914@users.noreply.github.com>
2024-01-23 15:51:32 -0500

    method to clean data

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import joblib
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder
 
 def main():
     st.title('Machine Learning Model Deployment')
@@ -8,6 +10,7 @@ def main():
     
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
+        cleanData(df)
         st.subheader('Uploaded Data:')
         st.write(df)
         model_choice = st.selectbox("Choose a model", ["Logistic Regression", "Decision Tree", "Random Forest"])
@@ -22,6 +25,49 @@ def main():
         
         st.subheader('Predictions:')
         st.write(predictions)
+
+
+def cleanData(df):
+    df['Sex'] = df['Sex'].map({'Male': 0, 'Female': 1})
+    df['MaritalStatus'] = df['MaritalStatus'].map({'Single': 0, 'Married': 1})
+    df = df[df['MonthClaimed'] != '0']
+    df['Month'] = pd.to_datetime(df['Month'], format='%b')
+    df['Month'] = df['Month'].dt.month
+    day_mapping = {
+    'Monday': 0,
+    'Tuesday': 1,
+    'Wednesday': 2,
+    'Thursday': 3,
+    'Friday': 4,
+    'Saturday': 5,
+    'Sunday': 6
+     }
+    df['DayOfWeek'] = df['DayOfWeek'].map(day_mapping)
+    df['MonthClaimed'] = pd.to_datetime(df['MonthClaimed'], format='%b')
+    df['MonthClaimed'] = df['MonthClaimed'].dt.month
+    df['DayOfWeekClaimed'] = df['DayOfWeekClaimed'].map(day_mapping)
+    le = LabelEncoder()
+    df['Make'] = le.fit_transform(df['Make'])
+    columns_to_encode = ['AccidentArea',
+         'Fault',
+          'PolicyType',
+           'VehicleCategory',
+            'VehiclePrice',
+           'Days_Policy_Accident',
+             'Days_Policy_Claim',
+           'PastNumberOfClaims',
+           'AgeOfVehicle',
+          'AgeOfPolicyHolder',
+          'PoliceReportFiled',
+          'WitnessPresent',
+         'AgentType',
+          'NumberOfSuppliments',
+        'AddressChange_Claim',
+        'NumberOfCars',
+        'BasePolicy']
+    le = LabelEncoder()
+    for column in columns_to_encode:
+    df[column] = le.fit_transform(df[column])
 
 if __name__ == '__main__':
     main()
